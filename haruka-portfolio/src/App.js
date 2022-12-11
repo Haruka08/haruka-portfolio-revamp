d2332aee5218ceae1bec0922327254a8cc910afd
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 22:19:50 +0000

    deleted portfoliocontainer.js and page folder. Changed to navigate from app.js, to navigation.js through header.js to render contents

--- haruka-portfolio/src/App.js
+++ haruka-portfolio/src/App.js
@@ -2,14 +2,12 @@
 // import './App.css';
 import React from 'react';
 import Header from "./component/Header"
-import PortfolioContainer from "./page/PortfolioContainer"
 import Footer from "./component/Footer"
 
 export default function App() {
   return (
     <div>
       <Header />
-        <PortfolioContainer />
       <Footer />
     </div>
   );
--- haruka-portfolio/src/component/Navigation.js
+++ haruka-portfolio/src/component/Navigation.js
@@ -1,15 +1,35 @@
 import React from "react";
-
+import About from '../component/About';
+import Contact from '../component/Contact';
+import Project from '../component/Project';
+import Resume from '../component/Resume';
 
 // A single Navigation component within the header that will be used to conditionally render the different sections of your portfolio
-export default function Navigation({ currentPage, handlePageChange }) {
+
+export default function Navigation() {
+    const [currentPage, setCurrentPage] = useState('Project');
+  
+    // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+    const renderPage = () => {
+      if (currentPage === 'Project') {
+        return <Project />;
+      }
+      if (currentPage === 'About') {
+          return <About />;
+      }
+      if (currentPage === 'Contact') {
+          return <Contact />;
+      }
+      return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
     return (
-        // <nav className="navbar navbar-expand-lg">
-        // <div className="container-fluid d-flex flex-row-reverse">
-        //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        //     <span className="navbar-toggler-icon"></span>
-        //     </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div>
+        {/* We are passing the currentPage from state and the function to update it */}
+        {/* <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} /> */}
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <a onClick={() => handlePageChange('Home')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
                     >About me</a>
@@ -24,40 +44,25 @@ export default function Navigation({ currentPage, handlePageChange }) {
                     >Resume</a>           
                 </div>
             </div>
-        // </div>
-        // </nav>
-    );
-  }
-
-
-function Header() {
-  const [currentPage, setCurrentPage] = useState('Project');
-
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === 'Project') {
-      return <Project />;
-    }
-    if (currentPage === 'About') {
-        return <About />;
-    }
-    if (currentPage === 'Contact') {
-        return <Contact />;
-    }
-    return <Resume />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
-  return (
-    <div>
-        
-        {/* We are passing the currentPage from state and the function to update it */}
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
         {/* Here we are calling the renderPage method which will return a component  */}
         {renderPage()}
-    </div>
-  );
+      </div>
+    );
 }
 
-export default Header;
+
+
+
+
+// export default function Navigation({ currentPage, handlePageChange }) {
+//     return (
+//         <nav className="navbar navbar-expand-lg">
+//         <div className="container-fluid d-flex flex-row-reverse">
+//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon"></span>
+//             </button>
+
+//         </div>
+//         </nav>
+//     );
+//   }
