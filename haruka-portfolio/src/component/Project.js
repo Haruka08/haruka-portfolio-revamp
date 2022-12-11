f970c76e452bd7dec725b0b55da57127d7969503
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 22:28:17 +0000

    changed class to className

--- haruka-portfolio/src/component/Project.js
+++ haruka-portfolio/src/component/Project.js
@@ -5,21 +5,21 @@ export default function Project() {
     return (
       <div>
 
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card" style="width: 18rem;">
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
-          <img src="..." class="card-img-top" alt="..."/>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div className="card" style="width: 18rem;">
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
 
