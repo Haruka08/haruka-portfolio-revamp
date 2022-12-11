d2838f69613f4562d07f5f1524f8a2e6706bf4d4
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 21:55:13 +0000

    amended Header.js to contain single navigation.js

--- haruka-portfolio/src/component/Header.js
+++ haruka-portfolio/src/component/Header.js
@@ -1,37 +1,48 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
-import About from './About';
-import Resume from './Resume';
-import Contact from './Contact';
+import About from '../page/About';
+import Resume from '../page/Resume';
+import Contact from '../page/Contact';
 import Project from '../page/Project';
 
 // A single Header component that appears on multiple pages
-export default function Header() {
-  const [currentPage, setCurrentPage] = useState('Project');
-
-  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-  const renderPage = () => {
-    if (currentPage === 'Project') {
-      return <Project />;
-    }
-    if (currentPage === 'About') {
-        return <About />;
-    }
-    if (currentPage === 'Contact') {
-        return <Contact />;
-    }
-    return <Resume />;
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
+function Header() {
   return (
     <div>
-        
-        {/* We are passing the currentPage from state and the function to update it */}
-        <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-        {/* Here we are calling the renderPage method which will return a component  */}
-        {renderPage()}
+      <Navigation />
     </div>
   );
 }
+
+export default Header;
+
+
+// export default function Header() {
+//   const [currentPage, setCurrentPage] = useState('Project');
+
+//   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+//   const renderPage = () => {
+//     if (currentPage === 'Project') {
+//       return <Project />;
+//     }
+//     if (currentPage === 'About') {
+//         return <About />;
+//     }
+//     if (currentPage === 'Contact') {
+//         return <Contact />;
+//     }
+//     return <Resume />;
+//   };
+
+//   const handlePageChange = (page) => setCurrentPage(page);
+
+//   return (
+//     <div>
+        
+//         {/* We are passing the currentPage from state and the function to update it */}
+//         <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+//         {/* Here we are calling the renderPage method which will return a component  */}
+//         {renderPage()}
+//     </div>
+//   );
+// }
