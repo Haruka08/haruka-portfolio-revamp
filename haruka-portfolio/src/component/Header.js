3bb30710221d6db26ddc78ffffff47736f1fe8be
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 21:28:09 +0000

    added bootstrap template to Resume.js

--- haruka-portfolio/src/component/Header.js
+++ haruka-portfolio/src/component/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
-import About from '../pages/About';
-import Resume from '../pages/Resume';
-import Contact from '../pages/Contact';
+import About from './About';
+import Resume from './Resume';
+import Contact from './Contact';
 import Project from './Project';
 
 // A single Header component that appears on multiple pages
