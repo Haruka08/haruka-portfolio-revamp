09c13e8c5c4db2678fe83f7f8f7dcf1ddb2515af
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-11 21:30:05 +0000

    added bootstrap template to project.js

--- haruka-portfolio/src/component/Project.js
+++ haruka-portfolio/src/component/Project.js
@@ -4,8 +4,25 @@ import React from "react";
 export default function Project() {
     return (
       <div>
-        <img src="..." class="rounded float-start" alt="..."/>
-        <img src="..." class="rounded float-end" alt="..."/>
+
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+
       </div>
     );
   }
