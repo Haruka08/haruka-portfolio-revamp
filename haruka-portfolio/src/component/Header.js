b4106ca0e3340d53dc73c4aa4b9c47a72c9303b6
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-12 00:52:11 +0000

    deleted components imports in header.js

--- haruka-portfolio/src/component/Header.js
+++ haruka-portfolio/src/component/Header.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
-import About from './About';
-import Resume from './Resume';
-import Contact from './Contact';
-import Project from './Project';
 
 // A single Header component that appears on multiple pages
 export default function Header() {
