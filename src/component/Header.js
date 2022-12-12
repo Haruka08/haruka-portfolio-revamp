3e8893e695e3e7a8a1a014d6e57fa0cc981622d4
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-12 11:48:59 +0000

    created header.css for header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react';
 import Navigation from './Navigation';
 
-const styles = {
-  headerStyle: {
-    background: 'Grey',
-    height: 50,
-  },
-  headingStyle: {
-    color: 'black',
-    padding: 10,
-  },
-};
+import '../css/header.css'
+
+// const styles = {
+//   headerStyle: {
+//     background: 'Grey',
+//     height: 50,
+//   },
+//   headingStyle: {
+//     color: 'black',
+//     padding: 10,
+//   },
+// };
 
 // A single Header component that appears on multiple pages
 export default function Header() {
   return (
     <div>
-      <header style={styles.headerStyle}>
-        <h2 style={styles.headingStyle}>Haruka's Portfolio</h2>
+      <header className="custom-header">
+        <h2 className="custom-h2">Haruka's Portfolio</h2>
       </header>
       <Navigation />
     </div>
