fa8890680db2d084ea67bb037f689df0f02dcc3a
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-12 10:31:40 +0000

    created about.css to about.js

--- src/component/About.js
+++ src/component/About.js
@@ -1,21 +1,11 @@
 import React from 'react';
-
-const styles = {
-    imgStyle: {
-      width: 370,
-      height: 500,
-    },
-    paragraphStyle: {
-      color: 'black',
-      padding: 10,
-    },
-  };
+import "../css/about.css"
 
 export default function About() {
   return (
-        <div>
-            <img style={styles.imgStyle} src={require("../images/haruka-photo.jpg")} alt="Haruka"></img>
-            <p>
+        <div id="about">
+            <img id="aboutImg" src={require("../images/haruka-photo.jpg")} alt="Haruka"></img>
+            <p id="aboutText">
                 Hi! Welcome to my portfolio, my name is Haruka.
                 I'm from Japan but spend over 20 years in the UK studying and working in automotive industry.
                 Since having a taste of programming through work using Microsoft Excel and VBA etc. I found myself enjoying the process and feeling exhilarated when the program worked as I intended!
@@ -25,3 +15,17 @@ export default function About() {
         </div>
     );
 }
+
+
+// const styles = {
+//   imgStyle: {
+//     width: 370,
+//     height: 500,
+//   },
+//   paragraphStyle: {
+//     color: 'black',
+//     padding: 10,
+//   },
+// };
+
+// style={styles.imgStyle}
